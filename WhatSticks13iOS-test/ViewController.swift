4f7d72bcf19bf229c284017457f7aa70883cde85
Nick Rodriguez <nrodrig1@gmail.com>
2024-07-03 11:26:22 +0200

    Initial Commit

---
+++ WhatSticks13iOS-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatSticks13iOS-test
+//
+//  Created by Nick Rodriguez on 03/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
